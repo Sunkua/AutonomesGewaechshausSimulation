a81e3ffe5d1a351656a40d6be57d990e7acef660
Benjamin Junker <s49827@beuth-hochschule.de>
2017-06-21 23:27:58 +0200

    Testing something with JUnit

---
+++ src_test/gewaechshaus/logic/AbladestationTest.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package gewaechshaus.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author benny
+ *
+ */
+public class AbladestationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link gewaechshaus.logic.Abladestation#Abladestation(gewaechshaus.logic.Position)}.
+	 */
+	@Test
+	public void testAbladestation() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link gewaechshaus.logic.Abladestation#updateFuellstand(int)}.
+	 */
+	@Test
+	public void testUpdateFuellstand() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link gewaechshaus.logic.Abladestation#getFuellstand()}.
+	 */
+	@Test
+	public void testGetFuellstand() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link gewaechshaus.logic.Abladestation#leeren()}.
+	 */
+	@Test
+	public void testLeeren() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link gewaechshaus.logic.Abladestation#getPflanzenart()}.
+	 */
+	@Test
+	public void testGetPflanzenart() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link gewaechshaus.logic.Abladestation#setPflanzenart(gewaechshaus.logic.PflanzenArt)}.
+	 */
+	@Test
+	public void testSetPflanzenart() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link gewaechshaus.logic.Abladestation#getStatus()}.
+	 */
+	@Test
+	public void testGetStatus() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link gewaechshaus.logic.Abladestation#getGridPosition()}.
+	 */
+	@Test
+	public void testGetGridPosition() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link gewaechshaus.logic.Abladestation#getAblagetyp()}.
+	 */
+	@Test
+	public void testGetAblagetyp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link gewaechshaus.logic.Abladestation#setAblagetyp(gewaechshaus.logic.AblageTyp)}.
+	 */
+	@Test
+	public void testSetAblagetyp() {
+		fail("Not yet implemented");
+	}
+
+}
