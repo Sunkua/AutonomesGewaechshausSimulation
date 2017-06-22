2a750b82e96d823892ceb6546b56a52b8ffa56e0
Nick Teschner <nick-teschner@outlook.com>
2017-06-22 14:51:23 +0200

    Unit-Test für Einzelpflanzen
    Korpus für Unit-Tests für Einzelpflanzen

---
+++ src_test/gewaechshaus/logic/EinzelpflanzeTest.java
@@ -0,0 +1,33 @@
+package gewaechshaus.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.*; 
+
+public class EinzelpflanzeTest {
+	Einzelpflanze einzelpflanzeOhne;
+	Einzelpflanze einzelpflanzeMit;
+	Position position = new Position(4, 4);
+	double gewicht = 1.3;
+
+    @Before
+    public void init() {
+    	einzelpflanzeOhne = new Einzelpflanze();
+    	einzelpflanzeMit = new Einzelpflanze(PflanzenArt.eGurke, position, gewicht, PflanzenStatus.eReif, new Date());
+    }
+    
+    @Test
+    public void erstelle_Einzelpflanze_ohne_Inhalt_ist_nicht_null() throws Exception {
+    	assertFalse(einzelpflanzeOhne.equals(null));
+    }
+
+    @Test
+    public void erstelle_Einzelpflanze_mit_Inhalt_ist_nicht_null() throws Exception {
+    	assertFalse(einzelpflanzeMit.equals(null));
+    }
+}
