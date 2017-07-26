718019906e40bff30efc281f5c5589f5ee09d50e
Nick Teschner <nick-teschner@outlook.com>
2017-07-26 23:13:11 +0200

    Rework of system test
    Rework of system test
    - some more precise asserts
    - better and shorter while loops

--- src_test/gewaechshaus/logic/SystemTest.java
+++ src_test/gewaechshaus/logic/SystemTest.java
@@ -1,13 +1,17 @@
 package gewaechshaus.logic;
 
-import org.junit.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
- * Dieser Test simuliert den perfekten Ablauf. Eine Pflanze wird gesetzt, wächst, es wird ein Auftrag zum ernten erstellt
- * und sobald der Inhalt an der Ablagestation ankommt ist alles wunderbar.
+ * Dieser Test simuliert den perfekten Ablauf. Eine Pflanze wird gesetzt,
+ * wächst, es wird ein Auftrag zum ernten erstellt und sobald der Inhalt an der
+ * Ablagestation ankommt ist alles wunderbar.
  */
 public class SystemTest {
-	
+
 	Pflanzenverwaltung pVerwaltung;
 	Roboterleitsystem leitSystem;
 	Auftragsgenerator auftragsgenerator;
@@ -15,70 +19,74 @@ public class SystemTest {
 	Uhr uhr;
 
 	@Before
-    public void init() {
-        Gitter gitter = new Gitter(12f, 12f, 12, 12);
+	public void init() {
+		Gitter gitter = new Gitter(12f, 12f, 12, 12);
 
-        pVerwaltung = new Pflanzenverwaltung(gitter);
+		pVerwaltung = new Pflanzenverwaltung(gitter);
 
-        uhr = new Uhr(200);
-        leitSystem = new Roboterleitsystem(gitter, uhr);
-        auftragsgenerator = new Auftragsgenerator(pVerwaltung, leitSystem, uhr);
-        
-        Position abladestelle = new Position(11f, 11f);
-        gitter.toKarthesisch(abladestelle);
-        abladestation = new Abladestation(abladestelle);
+		uhr = new Uhr(200);
+		leitSystem = new Roboterleitsystem(gitter, uhr);
+		auftragsgenerator = new Auftragsgenerator(pVerwaltung, leitSystem, uhr);
 
-        pVerwaltung.addObserver(leitSystem);
-        pVerwaltung.addObserver(gitter);
-        leitSystem.addObserver(gitter);
-        uhr.addObserver(leitSystem);
-        uhr.addObserver(pVerwaltung);
+		Position abladestelle = new Position(11f, 11f);
+		gitter.toKarthesisch(abladestelle);
+		abladestation = new Abladestation(abladestelle);
 
+		pVerwaltung.addObserver(leitSystem);
+		pVerwaltung.addObserver(gitter);
+		leitSystem.addObserver(gitter);
+		uhr.addObserver(leitSystem);
+		uhr.addObserver(pVerwaltung);
 
-        leitSystem.abladestationHinzufuegen(abladestation);
-        leitSystem.roboterHinzufuegen(pVerwaltung);
-        
-        abladestation.setAblagetyp(AblageTyp.eGut);
-        abladestation.leeren();
-    }
-	
+		leitSystem.abladestationHinzufuegen(abladestation);
+		leitSystem.roboterHinzufuegen(pVerwaltung);
+
+		abladestation.setAblagetyp(AblageTyp.eGut);
+		abladestation.leeren();
+	}
+
 	@Test
 	public void testSystem() {
-		
 		int a;
-		
-        try {
+
+		try {
 			pVerwaltung.pflanzeHinzufuegen(PflanzenArt.eTomate);
 		} catch (Exception e) {
-			Assert.fail("Fehler beim Hinzufügen der Pflanze!");
+			fail("Fehler beim Hinzufügen der Pflanze!");
 		}
-        
-        a = 0; // Safeguard, falls die Pflanze nicht wächst.
-        while ((pVerwaltung.holePflanzenVonStatus(PflanzenStatus.eReif).isEmpty() == true) && ( a < 1000))
-        {
-        	uhr.tick();
-        	a++;
-        }
-        
-        // Teste ob es wirklich nicht mehr leer ist oder er nur aus der Schleife geflogen ist
-        Assert.assertFalse(pVerwaltung.holePflanzenVonStatus(PflanzenStatus.eReif).isEmpty());
-        
-        leitSystem.auftragHinzufuegen(auftragsgenerator.pflanzenVonStatusErnten(PflanzenStatus.eReif));
-        
-        a = 0; // Safeguard, falls der Roboter nicht fährt.
-        while ((abladestation.getFuellstand() == 0.0) && (a < 1000))
-        {
-        	uhr.tick();
-        	a++;
-        }
-        
-        if (abladestation.getFuellstand() > 0.0)
-        {
-        	Assert.assertTrue(true);
-        }
-        else
-        {
-        	Assert.fail("Keine Früchte an der Ladestation erhalten");
-        }
+
+		// Mache die Pflanze schwerer, damit sie weggebracht werden muss)
+		double Tomatengewicht = Konstanten.maximalerFuellstand + 1;
+		pVerwaltung.holePflanzenVonStatus(PflanzenStatus.eUnreif).get(0).setGewicht(Tomatengewicht);
+		;
+
+		a = 0; // Safeguard, falls die Pflanze nicht wächst.
+		// So lange Pflanze wachsen lassen bis sie reif ist (Keine Unreifen mehr da
+		// sind)  
+		// Bei 0.2 Wachstum pro Tick, bei 90 = reif, dann ist die Pflanze ausgewachsen
+		double maxTicks = 90 / Konstanten.WachstumTomate + 30;
+
+		while (pVerwaltung.holePflanzenVonStatus(PflanzenStatus.eUnreif).isEmpty() == false && a <= maxTicks) {
+			uhr.tick();
+			a++;
+		}
+
+		// Teste ob es wirklich nicht mehr leer ist oder er nur aus der Schleife
+		// geflogen ist
+		assertFalse("Pflanze ist nicht gewachsen", pVerwaltung.holePflanzenVonStatus(PflanzenStatus.eReif).isEmpty());
+
+		leitSystem.auftragHinzufuegen(auftragsgenerator.pflanzenVonStatusErnten(PflanzenStatus.eReif));
+
+		a = 0; // Safeguard, falls der Roboter nicht fährt.
+		while ((abladestation.getFuellstand() == 0.0) && (a < 100)) {
+			uhr.tick();
+			a++;
+		}
+
+		// Die Pflanze wurde in der Abladestation abgegeben
+		assertEquals("Tomate wurde nicht geerntet - noch reife Tomate vorhanden", 0,
+				pVerwaltung.holePflanzenVonStatus(PflanzenStatus.eReif).size());
+		assertEquals("Tomate wurde nicht geerntet - keine geerntete Tomate vorhanden", 1,
+				pVerwaltung.holePflanzenVonStatus(PflanzenStatus.eGeerntet).size());
 	}
 }
